64c307b47051cb3c4287fd4ec98d1be40ce08c9f
Maria Rubinger <rubinger.maria@gmail.com>
2021-02-06 15:07:13 +0000

    Última página

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import BuscarEndereco from './components/BuscarEndereco';
-//import List from './List';
+import { Container, Typography, CardContent } from "@material-ui/core";
+import MapOutlinedIcon from '@material-ui/icons/MapOutlined';
 import './App.css';
 
 
 class App extends Component {
-  
   render(){
     return (
-      <>
-        <td>Encontre qualquer endereço do Brasil</td>
-          <BuscarEndereco />
-      </>
+      <Container component="article" maxWidth="sm">
+        <CardContent variant="outlined">
+          <MapOutlinedIcon style={{ fontSize: 60 }} color="secondary" />
+            <Typography variant="h5" component="h1" align="center">
+              Encontramos qualquer endereço do Brasil 
+            </Typography>
+            <BuscarEndereco validarCep={validarCep} />
+        </CardContent>
+      </Container>
     );
   }
 }
 
 
+function validarCep(){
+  var n = '/^[0-9]{8}$/'
+    if(n.length !== 8){
+      return {valido:false, texto:"O CEP deve ter 8 dígitos."}
+    }else{
+      return {valido:true, texto:""}
+    }
+}   
 
 export default App;
--- src/components/BuscarEndereco/index.js
+++ src/components/BuscarEndereco/index.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
 import axios from 'axios';
+import ReactDOM from 'react-dom';
 
-function BuscarEndereco() {
+
+function BuscarEndereco({ validarCep }) {
+  
   const [cep, setCep] = useState(""); //useState é um hook, o useState devolve um array com dois elementos
-  //cep vai ser a primeira variável que representa o estado
-  //setCep  vai ser a função que eu quero que ele use para ajustar aquele estado, para configurar ou atribuir um novo estado àquela variável
-  const [informacoes, setInformacoes] = useState({
+    //cep vai ser a primeira variável que representa o estado
+    //setCep  vai ser a função que eu quero que ele use para ajustar aquele estado, para configurar ou atribuir um novo estado àquela variável
+    const [erros, setErros] = useState({cep:{valido:true, texto:""}});
+    const [informacoes, setInformacoes] = useState({
     cep: '',
     logradouro: '',
     complemento: '',
@@ -14,59 +19,85 @@ function BuscarEndereco() {
     uf: '',
   });
 
+  
   const getInformacoes = () => {
     axios.get(`http://viacep.com.br/ws/${cep}/json/`)
-      .then(response => setInformacoes(response.data))
-
-    };
-
-/* 
-  const getInformacoes = () => {
-    axios.get(`http://viacep.com.br/ws/${cep}/json/`)
-    .then(response => response.json() 
       .then(response => {
-        setInformacoes(response.data)
-      }))
-     // .catch(() => { console.log('Erro'); });
-    }
-/* 
-    componentDidMount() {
-      this.refresh()
-  } */
-
+        //console.log(response.data)
+        if(response.data.erro){
+          const html = (
+          <div>
+            CEP inválido
+          </div>
+        );
+        ReactDOM.render(html,document.getElementById('result'));
+        } else{
+          const html = (
+            <div></div>
+          );
+          ReactDOM.render(html,document.getElementById('result'));
+          setInformacoes(response.data)
+        }
+      })
+      .catch((e) => { 
+        const html = (
+          <div>
+            Serviço indisponível
+          </div>
+        );
+        ReactDOM.render(html,document.getElementById('result'));
+        
+      });
+      //.catch((e) => { console.log(e); });
+    };
 
   return (
     <form
       onSubmit={(event) => { //arrow function anônima
         event.preventDefault(); //para prevenir o comportamento padrão do meu evento de Submit que é recarregar a página
-        console.log({ cep });
+        // console.log({ cep });
       }}
-    >
-      <label>
-        <input
+      >
+      <TextField
+          value={cep}
           onChange={(event) => {
-            setCep(event.target.value); //atribuindo meu estado ao valor do evento
+            setCep(event.target.value);
+            //atribuindo meu estado ao valor do evento
           }}
+          onBlur={(event) => {
+            const ehValido = validarCep(cep);
+            setErros({cep:ehValido})
+          }}
+          error={!erros.cep.valido}
+          helperText={erros.cep.texto}
+          label="CEP"
           type="text"
-          name="cep"
           placeholder="Digite seu CEP"
+          variant="outlined"
+          margin="normal"
+          fullWidth
         />
-      </label>
-      <button
+    
+      <Button
         type="submit"
-        value="Encontrar"
+        variant="contained"
+        color="secondary"
+       // value="Encontrar"
         onClick={getInformacoes}>
         Encontrar
-      </button>
+      </Button>
 
-      <ul>
-        <p>CEP: {informacoes.cep}</p>
-        <p>Logradouro: {informacoes.logradouro}</p>
-        <p>Complemento: {informacoes.complemento}</p>
-        <p>Bairro: {informacoes.bairro}</p>
-        <p>Localidade: {informacoes.localidade}</p>
-        <p>UF: {informacoes.uf}</p>
-      </ul>
+
+       <div id="result">
+         </div>   
+          <ul>
+            <p>CEP: {informacoes.cep}</p>
+            <p>Logradouro: {informacoes.logradouro}</p>
+            <p>Complemento: {informacoes.complemento}</p>
+            <p>Bairro: {informacoes.bairro}</p>
+            <p>Localidade: {informacoes.localidade}</p>
+            <p>UF: {informacoes.uf}</p>
+          </ul>
     </form>
   );
 }
